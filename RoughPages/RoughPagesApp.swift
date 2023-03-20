945e69bb70e154e7116ad27a18b14b05093eed09
Harvinder Singh <60150871+harvygr8@users.noreply.github.com>
2023-03-20 19:56:05 -0400

    Initial Commit

---
+++ RoughPages/RoughPagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoughPagesApp.swift
+//  RoughPages
+//
+//  Created by Harvinder Laliya on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct RoughPagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
